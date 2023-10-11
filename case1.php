606ffee081dac2f269b0ea19e1e4789aad7f4d21
Tierelantijntje <67382928+Tierelantijntje@users.noreply.github.com>
2023-10-11 10:37:59 +0200

    case1: Electricity Bill

---
+++ case1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Energy bill calculator</title>
+</head>
+<body>
+<form method="post">
+	<label for="input">Hoeveel energie heb je verbruikt in kW/h?</label>
+	<input type="number" id="energie" name="verbruik"><br>
+	<button type="submit">Bereken</button>
+</form><br>
+<?php
+$input = $_POST["verbruik"];
+if (isset($input)) {
+	bill($input); 
+}
+function bill($input){
+	if ($input <= 50){
+		$bill = $input * 3.5;
+	}	elseif ($input <= 150){
+		$bill = 175 + ($input - 50)*4;
+	}	elseif ($input <= 250){
+		$bill = 575 + ($input - 150)*5.2;
+	}	elseif ($input > 250){
+		$bill = 1095 + ($input - 250)*6.5;
+	}
+	echo "€" .$bill;
+}
+?>
+</body>
+</html>
