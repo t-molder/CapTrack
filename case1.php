4049db618dd02d5a478663dc1b28be24248ae39a
e10ne <etiennevanhalteren@hotmail.com>
2023-10-18 12:00:25 +0200

    Add spaces so that php code is more indented

--- case1.php
+++ case1.php
@@ -20,51 +20,48 @@
 			<br />
 			<p>
 	<?php
-	if (isset($_POST["verbruik"]) && !empty($_POST["verbruik"])) {
-		$input = $_POST["verbruik"];
-		$bill = bill($input); 
-		$tax = tax($input);
-		$results = array($bill, $tax);
-		echo "€" .$results[0]  ."€" .$results[1];
+		if (isset($_POST["verbruik"]) && !empty($_POST["verbruik"])) {
+			$input = $_POST["verbruik"];
+			$bill = bill($input); 
+			$tax = tax($input);
+			$results = array($bill, $tax);
+			echo "€" .$results[0]  ."€" .$results[1];
 
-	}
-
-	function bill($input){
-		if ($input <= 50){
-			$bill = $input * 3.5;
-		}	elseif ($input <= 150){
-			$bill = 175 + ($input - 50)*4;
-		}	elseif ($input <= 250){
-			$bill = 575 + ($input - 150)*5.2;
-		}	elseif ($input > 250){
-			$bill = 1095 + ($input - 250)*6.5;
 		}
-		return $bill;
+
+		function bill($input){
+			if ($input <= 50){
+				$bill = $input * 3.5;
+			}	elseif ($input <= 150){
+				$bill = 175 + ($input - 50)*4;
+			}	elseif ($input <= 250){
+				$bill = 575 + ($input - 150)*5.2;
+			}	elseif ($input > 250){
+				$bill = 1095 + ($input - 250)*6.5;
+			}
+			return $bill;
 		}
-		//echo "€" .$bill;
-	function tax($input){
-		$energytax = $input * 0.1125;
-		//echo "€" .$energytax;
-		return $energytax;
-	}
+	
+		function tax($input){
+			$energytax = $input * 0.1125;
+			return $energytax;
+		}
 
-
-	// $maandVerbruik array(
-	// 	'januari' => 'verbruik',
-	// 	'februari' => 'verbruik',
-	// 	'maart' => 'verbruik',
-	// 	'april' => 'verbruik',
-	// 	'mei' => 'verbruik',
-	// 	'juni' => 'verbruik',
-	// 	'juli' => 'verbruik',
-	// 	'augustus' => 'verbruik',
-	// 	'september' => 'verbruik',
-	// 	'oktober' => 'verbruik',
-	// 	'november' => 'verbruik',
-	// 	'december' => 'verbruik',
-	// 	'jaar' => 'verbruik'
-
-	// )
+		// $maandVerbruik array(
+		// 	'januari' => 'verbruik',
+		// 	'februari' => 'verbruik',
+		// 	'maart' => 'verbruik',
+		// 	'april' => 'verbruik',
+		// 	'mei' => 'verbruik',
+		// 	'juni' => 'verbruik',
+		// 	'juli' => 'verbruik',
+		// 	'augustus' => 'verbruik',
+		// 	'september' => 'verbruik',
+		// 	'oktober' => 'verbruik',
+		// 	'november' => 'verbruik',
+		// 	'december' => 'verbruik',
+		// 	'jaar' => 'verbruik'
+		// )
 		
 	?>
 			</p>
