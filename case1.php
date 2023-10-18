77bf0cc3084dc8c7c645dfd41018cc9c700d3e1f
e10ne <etiennevanhalteren@hotmail.com>
2023-10-18 12:01:35 +0200

    Add min to input

--- case1.php
+++ case1.php
@@ -13,7 +13,7 @@
 		<main>
 			<form method="post">
 				<label for="energie">Hoeveel energie heb je verbruikt in kW/h?</label>
-				<input type="number" id="energie" name="verbruik">
+				<input type="number" id="energie" name="verbruik" min="0">
 				<br />
 				<button type="submit">Bereken</button>
 			</form>
