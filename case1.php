355d160cbc146459cc69b2781e618f21fbf54215
bvb-tech <jacktechfora@proton.me>
2023-10-20 11:33:43 +0200

    working version

--- case1.php
+++ case1.php
@@ -11,60 +11,149 @@
 	</head>
 	<body>
 		<main>
-			<form method="post">
+
+		<div class="maincontainer">	
+	<div class="header1"><h3>&nbsp; &nbsp; &nbsp; Vul hier uw verbruik in&nbsp; &nbsp; &nbsp; </h3>
+	</div>
+
+	<div class="header2"><h3>&nbsp; &nbsp; &nbsp; Hieronder staat uw kosten overzicht &nbsp; &nbsp; &nbsp; </h3>
+	</div>
+	</div  >
+			<form class="energyInput" method="post">
+					<div class="period">       
+						<select required id="month" name="month">
+						<option value = "">Select month</option>
+						<?php
+							
+							for ($month = 1; $month <= 12; $month++) {
+								$monthName = date("F", mktime(0, null, null, $month, 1));
+								echo '<option value='.$month.'>'.$monthName.'</option>';
+								
+							}
+							
+						
+    					?>
+						</select>
+					</div>
+					<div class="energyUsage">
+						<input required type="number" name="period">
+						
+					</div>
+					<div class="submit">
+						<button type="submit">Bereken</button>
+					</div>	
+			</form><br>				
+	<div class="energycost">
+	<?php	
+
+						
+		function calc(){
+			
+			
+			
+			$month = "";
+			$energyCost = "";
+			$energyTax = 0;
+			if (isset($_POST['month'])) { $month = getMonth($_POST['month']); }
+			if (isset($_POST['period'])) { $energyCost = energyUsageCalc($_POST['period']);}
+			if (isset($_POST['period'])) { $energyTax = tax($_POST['period']); }
+
+			echo " Uw verbruik is " . "in" . " "  . $month . "is" ." " . $energyCost . "<br>";
+			echo " de betaalde belasting is" . " " . $energyTax;
+
+		}
+
+		function tax($value){
+			$energyTax = $value * 0.1125;
+			return $energyTax;
+			// echo " de betaalde belasting is" . " " . $energyTax;
+		}
+
+		function getMonth($value) {
+			// Get the right month name
+
+			for ($month = 1; $month <= 12; $month++) {
+				$monthName = date("F", mktime(0, null, null, $month, 1));
+				switch($value){
+					case 1;
+					$maand = "januari";
+					break;
+					case 2;
+					$maand = "februari";
+					break;
+					case 2;
+					$maand = "march";
+					break;
+					case 4;
+					$maand = "april";
+					break;
+					case 5;
+					$maand = "may";
+					break;
+					case 6;
+					$maand = "june";
+					break;
+					case 7;
+					$maand = "july";
+					break;
+					case 8;
+					$maand = "august";
+					break;
+					case 9;
+					$maand = "september";
+					break;
+					case 10;
+					$maand = "october";
+					break;
+					case 11;
+					$maand = "november";
+					break;
+					case 12;
+					$maand = "december";
+					break;
+				}
+				}
+				
+				return $maand;
+				
+			}		
+			
+		function energyUsageCalc($value) {
+			$energyUsage = $value;
+			if($energyUsage <= 50){
+				$energyCost = $energyUsage * 3.5;
+			}elseif($energyUsage > 50 && $energyUsage <= 150){
+				$energyCost = 175 + ($energyUsage - 50)* 4;
+
+			}elseif($energyUsage > 150 && $energyUsage <= 250){
+				$energyCost = 575 + ($energyUsage -150) * 5.2;
+			}elseif($energyUsage > 250){
+				$energyCost = 575 + ($energyUsage -250) * 6.5;
+			}
+			
+			return $energyCost;						
+		}	
+
+		if (isset($_POST)) {
+			calc();
+			
+		}
+
+		
+		
+		?>
+	</div>
+			<!-- <form method="post">
 				<label for="energie">Hoeveel energie heb je verbruikt in kW/h?
 				<input type="number" id="energie" name="verbruik" min="0" value="0">
 				</label>
 				<button type="submit">Bereken</button>
-			</form>
-	<?php
-		if (isset($_POST["verbruik"]) && !empty($_POST["verbruik"])) {
-			$input = $_POST["verbruik"];
-			$bill = bill($input); 
-			$tax = tax($input);
-			$results = array($bill, $tax);
-			foreach ($results as $result) {
-				echo "<p>€" . number_format($result, 2) . "</p>";
-			}
-			// echo "€" .$results[0]  ."€" .$results[1];
+			</form> -->
 
-		}
 
-		function bill($input){
-			if ($input <= 50){
-				$bill = $input * 3.5;
-			}	elseif ($input <= 150){
-				$bill = 175 + ($input - 50)*4;
-			}	elseif ($input <= 250){
-				$bill = 575 + ($input - 150)*5.2;
-			}	elseif ($input > 250){
-				$bill = 1095 + ($input - 250)*6.5;
-			}
-			return $bill;
-		}
 	
-		function tax($input){
-			$energytax = $input * 0.1125;
-			return $energytax;
-		}
-
-		// $maandVerbruik array(
-		// 	'januari' => 'verbruik',
-		// 	'februari' => 'verbruik',
-		// 	'maart' => 'verbruik',
-		// 	'april' => 'verbruik',
-		// 	'mei' => 'verbruik',
-		// 	'juni' => 'verbruik',
-		// 	'juli' => 'verbruik',
-		// 	'augustus' => 'verbruik',
-		// 	'september' => 'verbruik',
-		// 	'oktober' => 'verbruik',
-		// 	'november' => 'verbruik',
-		// 	'december' => 'verbruik',
-		// 	'jaar' => 'verbruik'
-		// )
 		
-	?>
+		
 		</main>
 	</body>
 </html>
